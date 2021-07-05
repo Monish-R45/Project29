5be10f759acb38200bfff50fcc43119c92a58f14
Monish-R45 <81694620+Monish-R45@users.noreply.github.com>
2021-07-05 16:23:44 +0530

    Update block.js

--- block.js
+++ block.js
@@ -1,7 +1,9 @@
 class Block{
     constructor(x, y, width, height) {
         var options = {
-           setStatic:false          
+           setStatic:false 
+           friction:0.0
+           restitution:0.4
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
